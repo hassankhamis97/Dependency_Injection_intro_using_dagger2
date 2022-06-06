09c6d4d13f0dcb1af00dd04ef9f6f112c7a6f31c
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2022-06-06 08:27:21 +0200

    Create PriceQualifier.kt

---
+++ app/src/main/java/com/hk/dependency_injection_intro/di/qualifiers/PriceQualifier.kt
@@ -0,0 +1,12 @@
+package com.hk.dependency_injection_intro.di.qualifiers
+
+import javax.inject.Qualifier
+
+/**
+ * Created by hassankhamis on 06,June,2022
+ */
+
+@Qualifier
+
+annotation class PriceQualifier {
+}
