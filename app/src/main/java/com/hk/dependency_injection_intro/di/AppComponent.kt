271b75df6a0cce023022fe08174abaffaf3a7064
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2022-06-06 09:23:11 +0200

    singleton annotation

--- app/src/main/java/com/hk/dependency_injection_intro/di/AppComponent.kt
+++ app/src/main/java/com/hk/dependency_injection_intro/di/AppComponent.kt
@@ -6,11 +6,12 @@ import com.hk.dependency_injection_intro.data.models.User
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Named
+import javax.inject.Singleton
 
 /**
  * Created by hassankhamis on 06,June,2022
  */
-
+@Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent {
     val product: Product
--- app/src/main/java/com/hk/dependency_injection_intro/di/AppModule.kt
+++ app/src/main/java/com/hk/dependency_injection_intro/di/AppModule.kt
@@ -3,6 +3,7 @@ package com.hk.dependency_injection_intro.di
 import com.hk.dependency_injection_intro.data.models.User
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 /**
  * Created by hassankhamis on 06,June,2022
@@ -10,6 +11,7 @@ import dagger.Provides
 
 @Module
 class AppModule {
+    @Singleton
     @Provides
     fun getUser(): User = User()
 }
