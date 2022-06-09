df9052f74e8a935be5046fef8f6eee5290fe8fbf
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2022-06-10 01:55:59 +0200

    Update MainActivity.kt

--- app/src/main/java/com/hk/dependency_injection_intro/MainActivity.kt
+++ app/src/main/java/com/hk/dependency_injection_intro/MainActivity.kt
@@ -12,8 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Inversion of control
-        val engine = Engine()
-        val tire = Tire()
-        val car = Car(engine, tire)
+        val engine1 = Engine()
+        val tire1 = Tire()
+        val car1 = Car(engine1, tire1)
+
+        val engine2 = Engine()
+        val tire2 = Tire()
+        val car2 = Car(engine2, tire2)
+
+        val engine3 = Engine()
+        val tire3 = Tire()
+        val car3 = Car(engine3, tire3)
+
+        val engine4 = Engine()
+        val tire4 = Tire()
+        val car4 = Car(engine4, tire4)
     }
 }
