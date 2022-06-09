48b2057f5183e7dc174a6f76a6fb861f3fc91ec9
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2022-06-10 01:53:51 +0200

    handle no di

--- app/src/main/java/com/hk/dependency_injection_intro/MainActivity.kt
+++ app/src/main/java/com/hk/dependency_injection_intro/MainActivity.kt
@@ -2,12 +2,19 @@ package com.hk.dependency_injection_intro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.hk.dependency_injection_intro.models.Car
+import com.hk.dependency_injection_intro.models.Engine
+import com.hk.dependency_injection_intro.models.Tire
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "Dependency_Injection_Intro_Using_Dagger2"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val car = Car.makeCar()
+        val car = Car()
+
+        Log.d(TAG, "onCreate: engine = ${car.engine}")
+        Log.d(TAG, "onCreate: tire = ${car.tire}")
     }
 }
--- app/src/main/java/com/hk/dependency_injection_intro/models/Car.kt
+++ app/src/main/java/com/hk/dependency_injection_intro/models/Car.kt
@@ -3,14 +3,11 @@ package com.hk.dependency_injection_intro.models
 /**
  * Created by hassankhamis on 09,June,2022
  */
-class Car(val engine: Engine, val tire: Tire) {
-    companion object {
-        fun makeCar(): Car {
-            val engine = Engine()
-            val tire = Tire()
-            val car = Car(engine, tire)
-            return car
-        }
+class Car() {
+    val engine: Engine
+    val tire: Tire
+    init {
+        engine = Engine()
+        tire = Tire()
     }
-
 }
